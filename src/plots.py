596840a25e8a37c42f213a65f3b1db94f5287de6
Devinml <devinlink8@gmail.com>
2020-07-22 07:19:56 -0600

    added plotting

---
+++ src/plots.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+plt.style.use('ggplot')
+
+df_29 = pd.read_csv('/home/devin/Documents/Galvanize/repos/Galvanize_Capstone_1/data/cleaned_data_29.csv')
+df_275 = pd.read_csv('/home/devin/Documents/Galvanize/repos/Galvanize_Capstone_1/data/cleaned_data_275.csv')
+
+mean_29 = df_29['Price'].mean()
+mean_275 = df_275['Price'].mean()
+
+
+
+sqrt_n_29 = np.sqrt(len(df_29['Price']))
+sqrt_n_275 = np.sqrt(len(df_275['Price']))
+
+std_29 = df_275['Price'].std()/sqrt_n_29
+std_275 = df_275['Price'].std()/sqrt_n_275
+
+x = np.linspace(2000,4000,2000)
+
+norm_29 = stats.norm(loc=mean_29,scale=std_29)
+norm_275 = stats.norm(loc=mean_275,scale=std_275)
+
+fig, ax = plt.subplots()
+
+ax.plot(x,norm_275.pdf(x),color='red')
+ax.plot(x,norm_29.pdf(x),color= 'blue')
+
+plt.show()
