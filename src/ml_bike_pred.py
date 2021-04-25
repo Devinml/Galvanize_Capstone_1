286ee3d8e59c1e1d5b2f9fe77b550ae64179685a
Devinml <devinlink8@gmail.com>
2021-04-25 15:14:21 -0600

    preliminary results looking good

--- src/ml_bike_pred.py
+++ src/ml_bike_pred.py
@@ -3,8 +3,10 @@ import pandas as pd
 from math import sqrt
 import matplotlib.pyplot as plt 
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def check_road(size):
     unwanted = ['45cm', '52cm', '54cm', '55cm', '56cm', '57cm']
@@ -13,16 +15,14 @@ def check_road(size):
     else:
         return True
 
-def read_data(fp):
+def read_data():
     df = pd.read_csv('data/cleaned_data.csv')
     df['Unwanted'] = df['Size'].apply(check_road)
     df = df[df['Unwanted']]
     df = df.drop('Unwanted', axis=1)
     df = df[(df['Currency'] == 'CAD') | (df['Currency'] == 'USD')]
     df = df.drop('Unnamed: 0', axis=1)
-    df = df.drop('Title', axis=1)
-    
-    
+
     dummies = pd.get_dummies(df['Condition'])
     matierial = pd.get_dummies(df['Material'])
     currency = pd.get_dummies(df['Currency'])
@@ -32,23 +32,43 @@ def read_data(fp):
     df = df.drop('Currency', axis=1)
     df = df.drop('Size', axis=1)
     df = pd.concat([df,dummies, size, matierial, currency], axis=1)
-    df = df[df['Price'] <= 5000]
+    df = df[df['Price'] <= 6000]
     df = df[df['Price'] >= 1000]
     return df
 
-def predictions_and_plots():
-    fp = os.path.join('data', 'cleaned_data.csv')
-    df = read_data(fp)
-    print(df.columns)
+def nlp(df=None):
+    if  df is None:
+        df = pd.read_csv('data/cleaned_data.csv')
+    tf_idf = TfidfVectorizer()
+    x= tf_idf.fit_transform(raw_documents=df['Title'])
+    
+    df = pd.DataFrame(x.toarray())
+    return df
+
+    # print(type(vals))
+    
+
+def predict():
+    df = read_data()
+    nlp_df = nlp(df)
+    df = df.drop('Title', axis=1)
+    df = pd.concat([df, nlp_df])
+    df = df.fillna(value=0)
     X = df.drop('Price', axis=1)
     y = df['Price']
-    print(df.head())
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
     rf = RandomForestRegressor()
     rf.fit(X=X_train, y=y_train)
     preds = rf.predict(X_test)
     print(sqrt(mean_squared_error(y_true=y_test, y_pred=preds)))
+    lin_reg = LinearRegression()
+    lin_reg.fit(X=X_train, y=y_train)
+    lin_reg_preds = lin_reg.predict(X_test)
+    
+    # make_plot(y_test=y_test, preds=preds)
 
+def make_plot(y_test, preds):
     fig, ax = plt.subplots()
     ax.scatter(y_test, preds)
     ax.set_xlabel('Test Values')
@@ -56,8 +76,26 @@ def predictions_and_plots():
     plt.show()
 
 
-if __name__ == '__main__':
-    predictions_and_plots()    
+def predictions_and_plots():
+    fp = os.path.join('data', 'cleaned_data.csv')
+    df = read_data(fp)
+    print(df.columns)
+    X = df.drop('Price', axis=1)
+    y = df['Price']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    rf = RandomForestRegressor()
+    rf.fit(X=X_train, y=y_train)
+    preds = rf.predict(X_test)
+    print(sqrt(mean_squared_error(y_true=y_test, y_pred=preds)))
+    make_plot(y_test=y_test,preds=preds)
 
+
+
+
+if __name__ == '__main__':
+    # predictions_and_plots()    
+    predict()
+    # nlp()
     
     
